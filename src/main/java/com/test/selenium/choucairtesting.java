f03e2a74a207a9f2b6e4934c9f058b19fa97d5db
Nancyutp20 <53459111+Nancyutp20@users.noreply.github.com>
2019-11-27 23:07:15 -0600

    ctesting

---
+++ src/main/java/com/test/selenium/choucairtesting.java
@@ -0,0 +1,14 @@
+package com.test.selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class choucairtesting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
